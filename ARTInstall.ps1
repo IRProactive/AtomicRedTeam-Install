bf4b592c9f23de87afadbafd6accdd31415a3dfd
IRProactive <37023600+IRProactive@users.noreply.github.com>
2023-10-23 09:38:36 -0600

    Create ARTInstall.ps1

---
+++ ARTInstall.ps1
@@ -0,0 +1,26 @@
+function Test-Administrator  
+{  
+    [OutputType([bool])]
+    param()
+    process {
+        [Security.Principal.WindowsPrincipal]$user = [Security.Principal.WindowsIdentity]::GetCurrent();
+        return $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
+    }
+}
+
+if(-not (Test-Administrator))
+{
+    Write-Error "This script must be executed as Administrator.";
+    exit 1;
+}
+
+$Logfile = $MyInvocation.MyCommand.Path -replace '\.ps1$', '.log'
+Start-Transcript -Path $Logfile
+
+if (Test-Path "C:\AtomicRedTeam\") {
+   Import-Module "C:\AtomicRedTeam\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1" -Force
+}
+else {
+  IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1'); Install-AtomicRedTeam -getAtomics -Force
+  Import-Module "C:\AtomicRedTeam\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1" -Force
+}
